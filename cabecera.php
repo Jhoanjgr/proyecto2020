56a7efc54570e973fbb31c372fcd09f7bb570876
Jhoan Gómez <jhoangomezjj@gmail.com>
2020-03-19 17:36:06 -0400

    agregamos cabecera.php

---
+++ cabecera.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>AULA VIRTUAL</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+		
+		<link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css">
+		<link rel="stylesheet" href="css/nouislider.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+		
+		
+  <div class="main-section">
+
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+<div class="col-md-12">
+		<nav class="navbar px-0 navbar-expand-lg navbar-light">
+							  <a class="navbar-brand" href="home.php">AULA VIRUTAL</a>
+							  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+							    <span class="oi oi-menu"></span>
+							  </button>
+
+							  <div class="collapse navbar-collapse" id="navbarsExample09">
+							    <ul class="navbar-nav ml-auto">
+							      <li class="nav-item d-flex active">
+							        <a class="nav-link d-flex align-items-center" href="#">Profesores <span class="sr-only">(current)</span></a>
+							      </li>
+							      <li class="nav-item d-flex active">
+							        <a class="nav-link d-flex align-items-center" href="#">Aulas</a>
+							      </li>
+							      <li class="nav-item d-flex active">
+							        <a class="nav-link d-flex align-items-center" href="#">Sección</a>
+							      </li>
+							      <li class="nav-item d-flex active">
+							        <a class="nav-link d-flex align-items-center" href="#">Grado</a>
+
+							      </li>	
+							   <li class="nav-item d-flex active">
+							        <a class="nav-link d-flex align-items-center" href="#">Evaluación</a>
+							      </li>
+									 <li class="nav-item d-flex active">
+							        <a class="nav-link d-flex align-items-center" href="#">Asignaturas</a>
+							      </li>						      
+							      <li class="nav-item d-flex dropdown">
+							        <a class="nav-link d-flex align-items-center dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							          Opción
+							        </a>
+							        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							          <a class="dropdown-item" href="#">Mantenimiento</a>
+							          <div class="dropdown-divider"></div>
+							          <a class="dropdown-item" href="#">Cerrar Sesión</a>
+							        </div>
+							      </li>
+							    </ul>
+							  </div>
+							</nav>
+						</div>
+		  </div>
+	  </nav>
+    <!-- END nav -->
+
+
+  	<section class="hero-wrap" style="background-image: url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">
+  		<div class="overlay"></div>
+  		<div class="container">
+  			  	 <section class="ftco-section ftco-section-2" id="navigationTabs" style="position: absolute; float: left 50px;margin-left: -40px; margin-top: 25px;">
+			<div class="container" style="position: absolute; float: left 50px;margin-left: -40px; margin-top: 25px;">
+				<div class="row">
+					<div class="col-lg-6 mb-5 mb-md-0"style="position: absolute; float: left 50px;margin-left: -40px; margin-top: 25px;">
+						<div class="row no-gutters tabulation"style="position: absolute; float: left 50px;margin-left: -40px; margin-top: 25px;"
+							<div class="col-md-4">
+								<ul class="nav nav-pills nav-fill d-md-flex d-block">
+								  <li class="nav-item text-left">
+								    <a class="nav-link active py-3" data-toggle="tab" href="#home"><span class="ion-ios-home mr-2"></span> Home</a>
+								  </li>
+								  <li class="nav-item text-left">
+								    <a class="nav-link py-3" data-toggle="tab" href="#menu1"><span class="ion-ios-person mr-2"></span> Profile</a>
+								  </li>
+								  <li class="nav-item text-left">
+								    <a class="nav-link py-3" data-toggle="tab" href="#menu2"><span class="ion-ios-mail mr-2"></span> Messages</a>
+								  </li>
+								</ul>
+							</div>
+						</div>
+					</div>
+</section>
+  			<div class="row description align-items-center justify-content-center">
+  				<div class="col-md-8 text-center">
+  					<div class="text">
+  						 <img src="images/logo.png" width="160">
+  						<h4 class="mb-5">¡Bienvenido al mundo virtual!</h4>
+
+  
+  						<!-- <div class="designed d-inline-block">
+  							<d class="d-flex align-items-center">
+	  							<div class="img" style="background-image: url(images/person.jpg);"></div>
+	  							<div class="ml-3"><p class="mb-0">Designed by: <span>Colorlib.com</span></p></div>
+  							</d>
+  						</div> -->
+  					</div>
+
+  				</div>
+  			</div>
+  		</div>
+  	</section>
